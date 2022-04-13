d1d312246f7e56169250639d6bb0f4cb2d0dfa5b
Ifeanyi Umeh <ifeanyi4umeh@gmail.com>
2022-04-13 15:37:27 +0100

    Added FAQ section

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import AboutSection from '../components/AboutSection';
 import ServicesSection from '../components/ServicesSection';
+import FaqSection from '../components/FaqSection';
 
 
 const AboutUs = () => {
@@ -9,6 +10,7 @@ const AboutUs = () => {
         <div>
             <AboutSection />
             <ServicesSection />
+            <FaqSection />
         </div>
 
     )
